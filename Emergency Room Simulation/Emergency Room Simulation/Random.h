a9dbc994005c6998bd79a23d84c599fcef3b0ac4
KaileyCozart <kaileync@comcast.net>
2018-04-21 20:51:17 -0700

    user_input() implementation

---
+++ Emergency Room Simulation/Emergency Room Simulation/Random.h
@@ -0,0 +1,33 @@
+#pragma once
+#ifndef RANDOM_H_
+#define RANDOM_H_
+
+/* Taken from airplane simulator */
+#include <cstdlib>
+#include <ctime>
+
+class Random {
+
+public:
+
+	Random() {
+		srand((unsigned int)std::time(0));
+	}
+
+	Random(int seed) {
+		srand(seed);
+	}
+
+	/** Returns a random integer in the range 0 – n. */
+	int next_int(int n) {
+		return int(next_double() * n);
+	}
+
+	/** Return a random double in the range 0 – 1. */
+	double next_double() {
+		return double(rand()) / RAND_MAX;
+	}
+
+};
+
+#endif
--- Emergency Room Simulation/Emergency Room Simulation/Simulation.h
+++ Emergency Room Simulation/Emergency Room Simulation/Simulation.h
@@ -27,8 +27,15 @@ public:
 		treated_patient_queue = new Patient();
 	}
 
-	void enter_data() {
-		std::cout << "Enter Data" << std::endl;
+	void user_input() {
+		std::cout << "Welcome to the Emergency Room Simulator for 273ville, population 2000." << std::endl;
+		int average_number_of_patients = read_int("Please enter the average number of patients that enter the emergency room per hour: ", 1, 60);
+		double rate_of_arrival = average_number_of_patients / 60.0;
+		int number_of_doctors = read_int("Please enter the number of doctors that are working: ", 1, 10);
+		int number_of_nurses = read_int("Please enter the number of nurses that are working: ", 1, 10);
+		int total_time = 60 * 24 * 7;
+		// Set the arrival rate so that the correct number of patients can be added to the untreated patient queue
+		// Push correct number of doctors and nurses into the vectors
 	}
 
 	void run_simulation() {
@@ -40,8 +47,21 @@ public:
 		}
 	}
 
-	void show_stats() {
-		std::cout << "Show Stats" << std::endl;
+	void show_result() {
+		std::cout << "The average visit time was " << std::endl; // average_visit_time
+		int menu_options = read_int("Press 0 to exit. Press 1 to see the treated residents.", 0, 1);
+		if (menu_options == 0) {
+			return 0;
+		}
+		else if (menu_options == 1) {
+			// Display treated patient names
+			// Let user choose patient by name
+			// Or let user leave screen
+			// Display that patient's information an loop back
+		}
+		else {
+			std::cout << "Uh oh, something didn't go right." << std::endl;
+		}
 	}
 
 };
